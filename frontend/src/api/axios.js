81fa33de682f25c8af0e990dd911d7958a7cc271
nani-tyson <nanityson10@gmail.com>
2025-08-03 20:07:40 +0530

    corrected the backend deployed url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rag-podcast.onrender.com/', // or your deployed backend URL
+  baseURL: 'https://rag-podcast.onrender.com', // or your deployed backend URL
   headers: {
     'Content-Type': 'application/json',
   },
