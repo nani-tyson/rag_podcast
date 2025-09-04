5f2fe4ab02b5266bee9439ca77ab0d4f5cf36304
nani-tyson <nanityson10@gmail.com>
2025-09-04 23:20:44 +0530

    allowed cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,11 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://127.0.0.1:5173",   # local frontend (dev)
+    "http://localhost:5173",   # another local alias
+    "https://rag-podcast.vercel.app/",  # replace with your deployed frontend domain
+]
 # CORS (allow frontend to call this backend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
